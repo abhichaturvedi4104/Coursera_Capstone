76bac9d53be1ecb7533a0d8e2d66f751f3bd2040
abhichaturvedi4104 <4104abhishek@gmail.com>
2020-04-27 17:38:04 +0530

    adds capstone project to github

---
+++ capstone_project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This Notebook will be used for Capstone project of the coursera __*data science specilaization*__ course
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
